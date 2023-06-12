ede07cc00510b9f8318c8de717e634471185c75a
trischevreau <95676814+trischevreau@users.noreply.github.com>
2023-06-12 10:33:51 +0200

    Better requirements validation

--- verify_requirements.py
+++ verify_requirements.py
@@ -4,14 +4,13 @@
 Tests the requirements for the use of this program.
 """
 
+import os
+
+from run_parameters import pytesseract_exe_file_path
+
+errors = 0
+
 libs = [
-    "os",
-    "pickle",
-    "math",
-    "tkinter",
-    "tkinter.filedialog",
-    "csv",
-    "shutil",
     "numpy",
     "matplotlib.pyplot",
     "cv2",
@@ -23,6 +22,20 @@ for lib in libs:
     try:
         exec(f"import {lib}")
     except ImportError:
-        print(f"library '{lib}' is missing or not installed correctly")
+        print(f"Library '{lib}' is missing or not installed correctly")
+        errors += 1
     else:
-        print(f"library '{lib}' OK")
+        print(f"Library '{lib}' OK")
+
+if not os.path.isfile(pytesseract_exe_file_path):
+    print(f"The Tesseract-OCR executable path may be incorrect : {pytesseract_exe_file_path}. \n"
+          f"Please change it in the run_parameters.")
+    errors += 1
+else:
+    print("The Tesseract-OCR executable path seems correct.")
+
+print()
+if errors == 0:
+    print("SUCCESS. The requirements are met !")
+else:
+    print(f"FAIL. {errors} error(s) occurred during requirements testing.")
